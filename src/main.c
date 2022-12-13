9b9beb4ad145d6492bf8bf256667282fbe695816
ulisesDCM <ulises.cmorales@hotmail.com>
2022-12-12 21:34:17 -0800

    Configured the LED0 to blink

--- src/main.c
+++ src/main.c
@@ -5,12 +5,32 @@
  */
 
 #include <zephyr/zephyr.h>
+#include <drivers/gpio.h>
+
+#define LED0_NODE 	DT_ALIAS(myled)
+
+static const struct gpio_dt_spec myLed = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 void main(void)
 {
-	while(1)
-	{
+	int ret = 0;
+
+	if(!device_is_ready(myLed.port)){
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&myLed, GPIO_OUTPUT_ACTIVE);
+	if(ret <0){
+		return;
+	}
+
+
+	while(1){
+		ret = gpio_pin_toggle_dt(&myLed);
+		if(ret <0){
+			return;
+		}
 		printk("hello world\r\n");
-		k_msleep(100);
+		k_msleep(1000);
 	}
 }
