5afe635ee15b2284aaf6c755133e0b2d35dd9ffc
ulisesDCM <ulises.cmorales@hotmail.com>
2022-12-14 18:43:13 -0800

    test github communicationw

--- src/main.c
+++ src/main.c
@@ -7,17 +7,30 @@
 #include <zephyr/zephyr.h>
 #include <drivers/gpio.h>
 
-/* LED nodes */
+/* BTN Nodes */
+#define BTN0_NODE		DT_ALIAS(btn0)
+#define BTN1_NODE		DT_ALIAS(btn1)
+#define BTN2_NODE		DT_ALIAS(btn2)
+#define BTN3_NODE		DT_ALIAS(btn3)
+
+/* LED Nodes */
 #define LED0_NODE 	DT_ALIAS(led0)
 #define LED1_NODE 	DT_ALIAS(led1)
 #define LED2_NODE 	DT_ALIAS(led2)
 #define LED3_NODE 	DT_ALIAS(led3)
 
-/* Led device structures */
+/* User LED device structures */
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 static const struct gpio_dt_spec led4 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
+
+/* User buttons device structures  */
+static const struct gpio_dt_spec btn0 = GPIO_DT_SPEC_GET(BTN0_NODE, gpios);
+static const struct gpio_dt_spec btn1 = GPIO_DT_SPEC_GET(BTN1_NODE, gpios);
+static const struct gpio_dt_spec btn2 = GPIO_DT_SPEC_GET(BTN2_NODE, gpios);
+static const struct gpio_dt_spec btn3 = GPIO_DT_SPEC_GET(BTN3_NODE, gpios);
+
 
 static int usr_led_init(void)
 {
@@ -28,7 +41,7 @@ static int usr_led_init(void)
 	device_is_ready(&led2);
 	device_is_ready(&led3);
 	device_is_ready(&led4);
-
+	//Test communication
 	//Configure GPIO pins as single outputs
 	ret = gpio_pin_configure_dt(&led1,  GPIO_OUTPUT | GPIO_OUTPUT_LOW);
 	ret |= gpio_pin_configure_dt(&led2, GPIO_OUTPUT | GPIO_OUTPUT_LOW);
@@ -36,6 +49,11 @@ static int usr_led_init(void)
 	ret |= gpio_pin_configure_dt(&led4, GPIO_OUTPUT | GPIO_OUTPUT_LOW);
 
 	return ret;
+}
+
+static int user_btn_init(void)
+{
+
 }
 
 void main(void)
